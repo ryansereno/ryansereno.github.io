5a03f500411f4525135e8a36d3e913681cfaf0eb
ryansereno <ryansereno@users.noreply.github.com>
2024-10-11 00:43:45 +0000

    deploy: 9c5a8ce2abd7915a48a7fca64a874288bdb55e27

--- assets/about.md.CC6Z2hCA.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a,M as i}from"./chunks/framework.D4geoam_.js";const h=JSON.parse('{"title":"About","description":"","frontmatter":{},"headers":[],"relativePath":"about.md","filePath":"about.md"}'),o={name:"about.md"},n=i("",7),l=[n];function s(r,d,c,m,p,u){return a(),t("div",null,l)}const b=e(o,[["render",s]]);export{h as __pageData,b as default};
